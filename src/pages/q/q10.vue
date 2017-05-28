8c208665c874450b836738a8f88168acbc0e173f
buchiya4th <buchiya4th@gmail.com>
2017-05-28 12:39:24 +0900

    リンク修正

--- src/pages/q/q10.vue
+++ src/pages/q/q10.vue
@@ -3,6 +3,6 @@
   p.questionText
     | ムードメーカーになりたい願望がある。
   ul.answerList
-    li: router-link(to="/q/q14/") はい
-    li: router-link(to="/q/q15/") いいえ
+    li: router-link(to="/q/q15/") はい
+    li: router-link(to="/q/q14/") いいえ
 </template>
